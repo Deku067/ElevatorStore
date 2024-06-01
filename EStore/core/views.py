0e6b8eea79c01167b6c2a464f8811be2b23a3c10
Deku067 <125663728+Deku067@users.noreply.github.com>
2024-06-01 15:22:51 +0330

    add tempelates to project and set local static files

--- EStore/core/views.py
+++ EStore/core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Part
 
-# Create your views here.
+def home(request):
+    return render(request, "core/home.html", {})
+
+def product_list(request):
+    parts = Part.objects.all()
+    return render(request, 'core/product_list.html', {'parts': parts})
+
+def aboutus(request):
+    return render(request, 'core/AboutUs.html')
+
+def contact(request):
+    return render(request, 'core/Contact.html')
+
